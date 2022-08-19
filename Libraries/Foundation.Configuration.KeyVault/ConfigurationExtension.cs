ba00c4353e8daabc3955a3517b35e0004a98cf71
Serge Berwert <serge@youranswer.ch>
2022-08-19 14:54:33 +0200

    move class

---
+++ Libraries/Foundation.Configuration.KeyVault/ConfigurationExtension.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Foundation.Logging.EventHubLogger.Configuration;
+
+public static class ConfigurationExtension
+{
+    public static KeyVaultConfiguration GetKeyVaultConfiguration(this IConfiguration config, string key = "Secrets")
+    {
+        return config.GetSection(key).Get<KeyVaultConfiguration>();
+    }
+}
--- Libraries/Foundation.Configuration.KeyVault/EventHubLoggerConfiguration.cs
+++ Libraries/Foundation.Configuration.KeyVault/EventHubLoggerConfiguration.cs
@@ -1,16 +1,6 @@
-﻿using Microsoft.Extensions.Configuration;
-
-namespace Foundation.Logging.EventHubLogger.Configuration;
+﻿namespace Foundation.Logging.EventHubLogger.Configuration;
 
 public class KeyVaultConfiguration
 {
     public string? KeyVaultUrl { get; set; }
-}
-
-public static class ConfigurationExtension
-{
-    public static KeyVaultConfiguration GetKeyVaultConfiguration(this IConfiguration config, string key = "Secrets")
-    {
-        return config.GetSection(key).Get<KeyVaultConfiguration>();
-    }
 }
