1e86d665606800f457632d30178c3e38c55e3b86
Serge Berwert <serge@youranswer.ch>
2022-08-23 14:46:48 +0200

    delete old code

--- Examples/Foundation.Example.WebAPI/Program.cs
+++ Examples/Foundation.Example.WebAPI/Program.cs
@@ -1,11 +1,8 @@
-using Azure.Identity;
 using Foundation.Configuration.KeyVault;
 using Foundation.Hosting.Info;
 using Foundation.Hosting.Kestrel.CertBinding;
 using Foundation.Logging.EventHubLogger;
 using Microsoft.Extensions.Azure;
-using Microsoft.Extensions.Configuration;
-using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,33 +16,13 @@ builder.WebHost.ConfigureKestrel(o =>
 
 builder.Configuration.AddAzureKeyVault();
 
-//builder.Services.AddAzureClients(builder =>
-//{
-//    // Register blob service client and initialize it using the KeyVault section of configuration
-//    builder.AddSecretClient(Configuration.GetSection("KeyVault"))
-//        // Set the name for this client registration
-//        .WithName("NamedBlobClient")
-//        // Set the credential for this client registration
-//        .WithCredential(new ClientSecretCredential("<tenant_id>", "<client_id>", "<client_secret>"))
-//        // Configure the client options
-//        .ConfigureOptions(options => options.Retry.MaxRetries = 10);
-
-//});
-
-
 builder.Services.AddAzureClients(builder =>
 {
-    builder.AddSecretClient(new Uri("http://my.keyvault.com"));
     builder.AddCertificateClient(new Uri("https://kv-fd-certificates.vault.azure.net/")).WithName("KV-FD-Certificates");
-    //builder.AddEventHubProducerClient(opts.ConnectionString).WithName("EventHubLogger");
 });
 
 
 builder.Logging.AddEventHubLogger();
-
-
-
-
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
