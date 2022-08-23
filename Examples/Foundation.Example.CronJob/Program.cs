515a1d3d7de2272548e71ecba1e3baeae12910d1
Serge Berwert <serge@youranswer.ch>
2022-08-23 12:01:49 +0200

    Add CronJob project

---
+++ Examples/Foundation.Example.CronJob/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Foundation.ServiceBuilder.AzureDefault;
+using Microsoft.Extensions.Azure;
+
+
+var stack = DefaultAzureStack.Create
+    .AddConfiguration()
+    .AddLogging()
+    .AddServices(s =>
+    {
+        s.AddAzureClients(e =>
+        {
+            
+        });
+
+        //s.AddPipeline(builder =>
+        //{
+        //    builder.AddCommand<ClearContainers>();
+        //});
+    });
+
+var provider = stack.Build();
+
+//Do Code
