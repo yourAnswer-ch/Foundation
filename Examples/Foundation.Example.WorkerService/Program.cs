3445a7ae7d00a2fcfc5bfb9dfda50757e4ae4965
Serge Berwert <serge@youranswer.ch>
2022-08-23 11:17:24 +0200

    update worker example

---
+++ Examples/Foundation.Example.WorkerService/Program.cs
@@ -0,0 +1,37 @@
+using Foundation.Configuration.KeyVault;
+using Foundation.Hosting.Info;
+using Foundation.Logging.EventHubLogger;
+using Foundation.Processing.StorageQueue;
+using Microsoft.Extensions.Azure;
+
+var builder = Host.CreateDefaultBuilder(args);
+
+var environment = Environment.GetEnvironmentVariable("ENVIRONMENT");
+
+if (string.IsNullOrWhiteSpace(environment))
+    environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+builder.ConfigureHostConfiguration(c =>
+{
+    c.AddJsonFile("appsettings.json", false);
+    if (!string.IsNullOrWhiteSpace(environment))
+        c.AddJsonFile($"appsettings.{environment}.json", true);
+    c.AddAzureKeyVault();
+});
+
+builder.ConfigureLogging(l =>
+{
+    l.AddEventHubLogger();
+});
+
+builder.ConfigureServices(s => {
+    s.AddAzureClients(builder =>
+    {
+        //builder.AddSecretClient(new Uri("http://my.keyvault.com"));
+        //builder.AddCertificateClient(new Uri("https://kv-fd-certificates.vault.azure.net/")).WithName("KV-FD-Certificates");
+    });
+
+    s.AddHostedService<QueueProcessorService>();
+});
+
+builder.Build().RegisterLifetimeLogger().Start();
---
+++ Libraries/Foundation.Configuration.Defaults/ConfigurationExtension.cs
@@ -0,0 +1,23 @@
+﻿using Foundation.Configuration.KeyVault;
+using Microsoft.Extensions.Configuration;
+
+namespace Foundation.Configuration.Defaults;
+
+public static class ConfigurationExtension 
+{
+    public static void AddDefaultConfiguration(IConfigurationBuilder builder)
+    {
+        var environment = Environment.GetEnvironmentVariable("ENVIRONMENT");
+
+        if (string.IsNullOrWhiteSpace(environment))
+            environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+        builder.AddJsonFile("appsettings.json", false);
+            
+        if (!string.IsNullOrWhiteSpace(environment))
+            builder.AddJsonFile($"appsettings.{environment}.json", true);
+
+        builder.AddEnvironmentVariables();
+        builder.AddAzureKeyVault();        
+    }
+}
