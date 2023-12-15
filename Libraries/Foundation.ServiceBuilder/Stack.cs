24fc0496a631917ed401db3e16effef2fcc8c498
Serge Berwert <serge@youranswer.ch>
2023-12-14 18:46:08 -0600

    add config to service builder

--- Libraries/Foundation.ServiceBuilder/Stack.cs
+++ Libraries/Foundation.ServiceBuilder/Stack.cs
@@ -71,6 +71,16 @@ public class Stack : IStack
         return this;
     }
 
+    public virtual IStack AddServices(Action<IServiceCollection, IConfiguration>? builder = null)
+    {
+        if (_configurationBuilder == null)
+            throw new ArgumentException("Stack - Add configuration bevore adding services.");
+
+        var configuration = (IConfiguration)_configurationBuilder.Build();
+        builder?.Invoke(_servicesCollection, configuration);
+        return this;
+    }
+
     public virtual IServiceProvider Build(Action<IServiceProvider>? initializer = null)
     {
         //if (_configurationBuilder?.Build() is IConfiguration configuration)
