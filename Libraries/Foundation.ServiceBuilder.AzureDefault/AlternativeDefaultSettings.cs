37e6e353f2b0588d8ce258c0f2464e9a83f3d10d
Serge Berwert <serge@youranswer.ch>
2024-02-18 01:10:57 +0100

    add alternative config

---
+++ Libraries/Foundation.ServiceBuilder.AzureDefault/AlternativeDefaultSettings.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Foundation.ServiceBuilder.AzureDefault;
+
+public static class AlternativeDefaultSettings
+{
+    public static IStack AddLoggingWithoutEventHubLogger(this IStack stack)
+    {
+        return stack.AddLoggingWithoutEventHubLogger(_ => { });
+    }
+
+    public static IStack AddLoggingWithoutEventHubLogger(this IStack stack, Action<ILoggingBuilder, IConfiguration>? builder)
+    {
+        return stack.AddLogging((b, c) =>
+        {
+            b.AddConsole();
+            b.AddTraceSource("Sherlock");
+            b.AddConfiguration(c.GetSection("Logging"));
+            builder?.Invoke(b, c);
+        });
+    }
+
+    public static IStack AddLoggingWithoutEventHubLogger(this IStack stack, Action<ILoggingBuilder>? builder)
+    {
+        return stack.AddLogging((b, c) =>
+        {
+            b.AddConsole();
+            b.AddTraceSource("Sherlock");
+            b.AddConfiguration(c.GetSection("Logging"));
+            builder?.Invoke(b);
+        });
+    }
+}
