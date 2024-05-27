b6700e090a3464af68c2a4e3b5d3cbd3854a9936
Serge Berwert <serge@youranswer.ch>
2024-05-27 22:48:53 +0200

    App credential parameter

--- Libraries/Foundation.Configuration.KeyVault/ConfigurationExtensions.cs
+++ Libraries/Foundation.Configuration.KeyVault/ConfigurationExtensions.cs
@@ -1,19 +1,24 @@
-﻿using Azure.Identity;
+﻿using Azure.Core;
+using Azure.Core.Diagnostics;
+using Azure.Identity;
 using Foundation.Logging.EventHubLogger.Configuration;
 using Microsoft.Extensions.Configuration;
 
-namespace Foundation.Configuration.KeyVault
+namespace Foundation.Configuration.KeyVault;
+
+public static class ConfigurationExtensions
 {
-    public static class ConfigurationExtensions
+    public static IConfigurationBuilder AddAzureKeyVault(this IConfigurationBuilder builder, TokenCredential? credentials = null)
     {
-        public static IConfigurationBuilder AddAzureKeyVault(this IConfigurationBuilder builder)
-        {
-            var config = builder.Build().GetKeyVaultConfiguration();
+        var config = builder.Build().GetKeyVaultConfiguration();
 
-            if (config?.KeyVaultUrl == null)
-                throw new ArgumentException("Configuration - KeyVault host url not configured. Local config files needs to be added bevore KeyVault secres source.");
+        if (config?.KeyVaultUrl == null)
+            throw new ArgumentException("Configuration - KeyVault host url not configured. Local config files needs to be added bevore KeyVault secres source.");
 
-            return builder.AddAzureKeyVault(new Uri(config.KeyVaultUrl), new DefaultAzureCredential());
-        }
+        using AzureEventSourceListener listener = AzureEventSourceListener.CreateConsoleLogger();
+
+        return builder.AddAzureKeyVault(
+            new Uri(config.KeyVaultUrl),
+            credentials ?? new DefaultAzureCredential());
     }
 }
