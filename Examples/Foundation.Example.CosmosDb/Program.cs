fcb1c18aaded4d484296d63b0e82d7a88d06f5c0
Serge Berwert <serge@youranswer.ch>
2024-12-31 21:00:46 +0100

    Add QueryExtensions

--- Examples/Foundation.Example.CosmosDb/Program.cs
+++ Examples/Foundation.Example.CosmosDb/Program.cs
@@ -1,33 +1,142 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 using Foundation.CosmosDb;
 using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Serialization.HybridRow.Schemas;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Linq;
 
 
-Console.WriteLine($"New guid ID: {Id.NewBase62Id}");
+//Console.WriteLine($"New guid ID: {Id.NewBase62Id}");
 
-var configuration = new ConfigurationBuilder()
-    .AddUserSecrets<Program>()
-    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-    .Build();
+//var configuration = new ConfigurationBuilder()
+//    .AddUserSecrets<Program>()
+//    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+//    .Build();
 
-Console.WriteLine(configuration.GetDebugView());
+//Console.WriteLine(configuration.GetDebugView());
 
-var services = new ServiceCollection();
+//var services = new ServiceCollection();
 
-services.AddCosmosDb(o => configuration.Bind("Azure:CosmosDB:Flowcpt", o));
-services.AddCosmosDbContainer<TestContainer>();
-
-
-IServiceProvider provider = services.BuildServiceProvider();
+//services.AddCosmosDb(o => configuration.Bind("Azure:CosmosDB:Flowcpt", o));
+//services.AddCosmosDbContainer<TestContainer>();
 
 
-var db = provider.GetRequiredService<TestContainer>();
-var conatiner = await db.GetOrCreateContainer();
+//IServiceProvider provider = services.BuildServiceProvider();
+
+
+//var db = provider.GetRequiredService<TestContainer>();
+//var conatiner = await db.GetOrCreateContainer();
+
+//var container = provider.GetRequiredService<TestContainer>();
+
+//// Test 1
+//var query = new QueryDefinition("SELECT * FROM c");
+
+//var result = container.QueryAsync<JObject>(query);
+
+//await foreach (var item in result)
+//{
+//    Console.WriteLine(item);
+//}
+
+
+// Test 2
+//BenchmarkRunner.Run()
+
+//var container2 = await container.GetOrCreateContainer();
+//var query2 = new QueryDefinition("SELECT * FROM c");
+//var result2 = container2.GetItemQueryIterator<JObject>(query2);
+
+//while (result2 != null && result2.HasMoreResults)
+//{
+//    foreach (JObject document in await result2.ReadNextAsync())
+//    {
+//        Console.WriteLine(document);
+//    }
+//}
+
+BenchmarkRunner.Run<TestClass>();
+
+
+
 
 Console.WriteLine("Hello, World!");
+
+[MemoryDiagnoser]
+public class TestClass
+{
+    TestContainer container;
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddUserSecrets<Program>()
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+
+        Console.WriteLine(configuration.GetDebugView());
+
+        var services = new ServiceCollection();
+
+        services.AddCosmosDb(o => configuration.Bind("Azure:CosmosDB:Flowcpt", o));
+        services.AddCosmosDbContainer<TestContainer>();
+
+
+        IServiceProvider provider = services.BuildServiceProvider();
+
+        container = provider.GetRequiredService<TestContainer>();
+    }
+
+    [Benchmark]
+    public async Task RunTest1()
+    {
+        var query = new QueryDefinition("SELECT * FROM c");
+
+        var result = container.QueryAsync<JObject>(query);
+
+        var list = new List<JObject>();
+
+        await foreach (var item in result)
+        {
+            list.Add(item);
+            //Console.WriteLine(item);
+        }
+    }
+
+    [Benchmark]
+    public async Task RunTest2()
+    {
+        var container2 = await container.GetOrCreateContainer();
+        var query2 = new QueryDefinition("SELECT * FROM c");
+        var result2 = container2.GetItemQueryIterator<JObject>(query2);
+
+        var list = new List<JObject>();
+
+        while (result2 != null && result2.HasMoreResults)
+        {
+            foreach (JObject document in await result2.ReadNextAsync())
+            {
+                list.Add(document);
+                //Console.WriteLine(document);
+            }
+        }
+    }
+
+    [Benchmark]
+    public async Task RunTest3()
+    {
+        var query = new QueryDefinition("SELECT * FROM c");
+
+        var result = container.QueryAsync<JObject>(query);
+
+        var list = await result.ToListAsync();
+    }
+}
 
 
 
@@ -37,7 +146,7 @@ public class TestContainer(ICosmosDb db) : CosmosContainer(db)
     {
         return new ContainerProperties
         {
-            Id = "TestContainer",
+            Id = "Document",
             PartitionKeyPath = "/partitionId"
         };
     }
---
+++ Libraries/Foundation.CosmosDb/QueryExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Azure.Cosmos;
+
+
+namespace Foundation.CosmosDb;
+
+public static class QueryExtensions
+{
+    //public static async IAsyncEnumerable<T> GetDocuments<T>(this ICosmosDbContainer container, string query, CancellationToken cancellationToken = default)
+    //{
+    //    var current = await container.GetOrCreateContainer();
+
+    //    current.GetItemQueryIterator<T>(query);
+    //}
+
+    public static async IAsyncEnumerable<T> QueryAsync<T>(this ICosmosDbContainer container, QueryDefinition query, string? partitionKey = null)
+    {
+        var current = await container.GetOrCreateContainer();
+        var options = partitionKey != null ? new QueryRequestOptions
+        {
+            PartitionKey = new PartitionKey(partitionKey)
+        } : null;
+
+        var result = current.GetItemQueryIterator<T>(query, requestOptions: options);
+
+        while (result != null && result.HasMoreResults)
+        {
+            foreach (T document in await result.ReadNextAsync())
+            {
+                yield return document;
+            }
+        }
+    }
+}
